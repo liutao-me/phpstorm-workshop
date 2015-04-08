77bbf716dd6082d21340f4e6800ef8bca7b34c6f
wumouse <wumouse@qq.com>
2015-04-08 22:03:31 +0800

    05 - 06 Done

--- 05 - Refactoring/06 - Extract Interface.php
+++ 05 - Refactoring/06 - Extract Interface.php
@@ -9,15 +9,16 @@
 
 namespace Refactoring6\JetBrains;
 
-// 1. Extract an interface from the CustomerRepository using Refactor This.
-//    Name the interface IRepository and make sure it is in the JetBrains\Contracts namespace.
-//    Copy PHPDoc blocks and make sure the get(), getAll(), save() and delete() methods are extracted.
-//    Checking the "Replace Class references with interfaces if possible" will update the CustomerController class.
+// 1. 使用 Refactor This菜单从 CustomerRepository 中提取一个接口。
+//    将该接口命名为 IRepository 并保证它在 JetBrains\Contracts 命名空间下。
+//    勾选 PHPDoc 选项为 Copy，并保证 get(), getAll(), save() and delete()函数被提取。
+//    勾选"Replace Class references with interfaces if possible"(如果可能使用接口替换类的引用)
+//    将会更新 CustomerController类。(看步骤2)
 //
-//    Apply the refactoring.
+//    应用该重构
 
 class CustomerController {
-    // 2. Verify the PHPDoc block has been updated and IRepository is in there.
+    // 2. 确认这里的PHPDoc块已经更新为 IRepository
     /** @var CustomerRepository */
     protected $_repository;
 
