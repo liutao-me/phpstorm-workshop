da44ab20e7981552e6e2dccc7beadb39c8d6fdc7
wumouse <wumouse@qq.com>
2015-04-08 22:38:39 +0800

    05 - 09 Done

--- 05 - Refactoring/09 - Extract Variable.php
+++ 05 - Refactoring/09 - Extract Variable.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Extract Variable
+ * 提取变量
  *
- * Put the result of a selected expression into a variable. The original expression is replaced with the new variable.
+ * 提取表达式的结果到一个变量。原来的表达式被替换为新的变量。
  *
  * Ctrl+Alt+V (Windows/Linux)
  * Alt+Command+V (Mac OS X)
@@ -15,9 +15,9 @@ class Blog {
     protected $_blogSubtitle = 'Just another blog';
 
     public function renderHeader() {
-        // 1. Place the caret on _blogSubtitle.
-        //    Extract the "{$this->_blogName} - {$this->_blogSubtitle}" string into a variable called $title.
-        //    Note the IDE asks us which expression should be extracted. Select the entire string.
+        // 1. 将光标放在 _blogSubtitle上。
+        //    提取 "{$this->_blogName} - {$this->_blogSubtitle}" 这段字符串到一个叫 $title的变量。
+        //    注意IDE会询问我们要提取哪个表达式。选择全部字符串。还有，勾选"Replace all occurrences"来替换第二段相同的字符串。
         echo '<title>';
         echo "{$this->_blogName} - {$this->_blogSubtitle}";
         echo '</title>';
